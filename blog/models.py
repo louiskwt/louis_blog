ffd32e23c0a69eceb470dfb55528374d61f65f32
Louis Tsang <kawingt2@gmail.com>
2022-07-21 22:33:55 +0800

    set up view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Blog model
 class PublishedManager(models.Manager):
@@ -22,6 +23,9 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager() # The default manager
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
