4f25053b5b2906cc40f055884b6c7a084aefe90e
Louis Tsang <kawingt2@gmail.com>
2022-08-13 00:08:27 +0800

    set up postgres in django

--- dev_blog/settings.py
+++ dev_blog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'dev_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PW'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
